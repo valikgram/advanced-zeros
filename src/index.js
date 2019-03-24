c172bccaa31d1202f6b695826f3f9ed392ecc906
valikgram <work.v@mail.ru>
2019-03-24 23:59:14 +0300

    add cycle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var raz = Number.MAX_VALUE;
+  var j = base;
+  for (var i = 2; i <= base; i++) {
+    if (j % i === 0) {
+      var p = 0;
+      while (j % i === 0) {
+         j = j / i;     
+         p++;
+      }
+      var c = 0;
+      var z = Math.floor(number / i);
+      while (z > 0) {
+        c += z;
+        z = Math.floor(z / i);
+      }
+      raz = Math.min(raz, Math.floor(c / p))
+    }
+  }
+  return raz;
 }
